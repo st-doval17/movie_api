044619ced0b9cadf19d203f5727e22f4613a3ebe
G.S <131451577+st-doval17@users.noreply.github.com>
2023-08-08 21:31:08 +0200

    Update Code
    - update code in index.js file

--- index.js
+++ index.js
@@ -44,9 +44,9 @@ app.use(
     origin: (origin, callback) => {
       if (!origin) return callback(null, true);
       if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
+        // If a specific origin isn't found on the list of allowed origins
         let message =
-          "The CORS policy for this application doesn’t allow access from origin " +
+          "The CORS policy for this application doesn't allow access from origin " +
           origin;
         return callback(new Error(message), false);
       }
@@ -155,7 +155,6 @@ app.get(
     const userName = req.params.userName;
 
     Users.findOne({ Username: userName }, "FavoriteMovies")
-      .populate("FavoriteMovies")
       .then((favoriteMovies) => {
         res.send(favoriteMovies.FavoriteMovies);
       })
@@ -220,13 +219,33 @@ app.post(
         { Username: req.params.userName },
         {
           $addToSet: { FavoriteMovies: req.params.MovieID },
-        }
+        },
+        { new: true }
       );
-      res.json(updatedUser);
+      res.json(updatedUser.FavoriteMovies);
     } catch (err) {
       console.error(err);
       res.status(500).send("Error: " + err);
     }
+  }
+);
+
+app.delete(
+  "/users/:userName/movies/:MovieID",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Users.findOneAndUpdate(
+      { Username: req.params.userName },
+      { $pull: { FavoriteMovies: req.params.MovieID } },
+      { new: true }
+    )
+      .then((updatedUser) => {
+        res.json(updatedUser.FavoriteMovies);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
   }
 );
 
