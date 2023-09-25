83f1f2cf712e90220c889794d89ddfd485cd2329
G.S <131451577+st-doval17@users.noreply.github.com>
2023-09-25 16:42:58 +0200

    Add JSDoc
    - add JSDoc to project
    - add JSDoc Block Commentary
    - update index.js
    - update auth.js

--- auth.js
+++ auth.js
@@ -1,26 +1,34 @@
-const jwtSecret = "your_jwt_secret"; //same key used in the JWTStrategy
+const jwtSecret = 'your_jwt_secret'; // The secret key used in the JWTStrategy
 
-const jwt = require("jsonwebtoken"),
-  passport = require("passport");
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
-require("./passport"); //local passport file
+require('./passport'); // Include the local passport file
 
+/**
+ * Generates a JWT token for a user.
+ * @param {object} user - The user object to encode in the JWT.
+ * @returns {string} JWT token.
+ */
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
-    subject: user.Username, // This is the username you’re encoding in the JWT
-    expiresIn: "7d", //  Specifies that the token will expire in 7 days
-    algorithm: "HS256", // This is the algorithm used to “sign” or encode the values of the JWT
+    subject: user.Username, // This is the username encoded in the JWT
+    expiresIn: '7d', // Token will expire in 7 days
+    algorithm: 'HS256', // The algorithm used to sign the JWT
   });
 };
 
-/* POST login. */
+/**
+ * Handles the POST request for user login.
+ * @param {object} router - The Express router.
+ */
 module.exports = (router) => {
-  router.post("/login", (req, res) => {
-    passport.authenticate("local", { session: false }, (error, user, info) => {
+  router.post('/login', (req, res) => {
+    passport.authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
         console.log(error);
         return res.status(400).json({
-          message: "Something is not right",
+          message: 'Something is not right',
           user: user,
         });
       }
